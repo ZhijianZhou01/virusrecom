5f3dd84ffebe17ba4bc11a456ce7caf20707694a
Zhou ZJ <43747306+ZhijianZhou01@users.noreply.github.com>
2023-08-10 16:38:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,26 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
   name="virusrecom",
-  version="1.1.0.0",
+  version="1.1.0.1",
   author="Zhi-Jian Zhou",
   author_email="zjzhou@hnu.edu.cn",
   description="An information-theory-based method for recombination detection of viral lineages.",
-  keywords='recombination', 
+  keywords="recombination, virus, evolution, information entropy",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/ZhijianZhou01/virusrecom",
   packages=setuptools.find_packages(),
   install_requires=["matplotlib","pandas","numpy", "scipy"],
-  include_package_data=True,
-  
+
   classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Mathematics",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
