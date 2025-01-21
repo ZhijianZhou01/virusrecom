2c1a5e6077e32f28de2ac4819aa0809157c1049e
Zhou ZJ <43747306+ZhijianZhou01@users.noreply.github.com>
2025-01-21 21:09:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
   name="virusrecom",
-  version="1.3.6",
+  version="1.3.7",
   author="Zhi-Jian Zhou",
   author_email="zjzhou@hnu.edu.cn",
   description="An information-theory-based method for recombination detection of viral lineages.",
